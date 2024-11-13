e50c95ea6ed5aeb1b2f134fd6a897e04d6bef0d6
Artem Mkrtchyan <a.s.mkrtchyan@gmail.com>
2024-11-13 11:05:30 -0700

    Initial Commit

---
+++ SoundCheckiOSTests/SoundCheckiOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  SoundCheckiOSTests.swift
+//  SoundCheckiOSTests
+//
+//  Created by Artem Mkrtchyan on 11/13/24.
+//
+
+import Testing
+@testable import SoundCheckiOS
+
+struct SoundCheckiOSTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
